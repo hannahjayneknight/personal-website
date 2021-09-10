dafdd42277d8307d4d2c41a6c8702cd746ab8e0b
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-09-10 13:43:24 +0100

    update to bio, experience and uni yr

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
-
+/*jslint devel: true */
 import express from "express";
-//import expressWS from "express-ws";
-
-//function ignorparam() {}
-
 const port = 8080;
-
 const app = express();
-//expressWS(app);
+
 
 app.disable("x-powered-by");
 
 app.use("/", express.static(""));
 
-// THESE ARE FOR WEB SOCKETS
-/*
-app.use(function (req, res, next) {
-    ignorparam(res);
-    req.testing = "testing";
-    return next();
-});
-
-app.get("/", function (req, res, next) {
-    ignorparam(next);
-    ignorparam(req);
-    res.end();
-});
-*/
-
-
-
-/////////////////////////////////////////////////////////
-
-
-
 app.listen(port, function () {
     console.log("Listening on port " + port);
 });
+
